7ea94dbbdbb313b2c07aa7693a9374237dd4a8c2
Christian <christian.mchugh@unt.edu>
2012-08-10 14:13:40 -0500

    Fix searching for fqdn when not called from the command line with mcollective Refacter to move searching logic into functions Add search for smartvars Add required configuration options Document configuration

--- lib/hiera/backend/foreman_backend.rb
+++ lib/hiera/backend/foreman_backend.rb
@@ -1,5 +1,6 @@
 # Foreman Backend for Hiera
 # Many thanks to @ohadlevy
+# from: https://github.com/torrancew/hiera-foreman
 
 class Hiera
   module Backend
@@ -9,31 +10,81 @@ class Hiera
       def initialize
         require 'net/http'
         require 'net/https'
+        require 'timeout'
+        require 'uri'
+        require 'json'
 
         @url = Config[:foreman][:url]
+        @user = Config[:foreman][:user]
+        @pass = Config[:foreman][:pass]
         Hiera.debug("Hiera Foreman backend starting")
       end
 
       def lookup(key, scope, order_override, resolution_type)
-        Hiera.debug("Looking up #{key} in Foreman backend")
+        begin
+          fqdn = scope.catalog.tags[4]
+        rescue
+          fqdn = scope['fqdn'] if scope.has_key?('fqdn')
+          Hiera.debug("trying mcollective")
+        end
+        Hiera.debug("got fqdn #{fqdn}")
+
+        results = []
         
-        fqdn = scope['fqdn'] if scope.has_key?('fqdn')
+        Hiera.debug("Trying #{key} in ENC")
+        results = encquery(fqdn, key)
+        if !results
+          Hiera.debug("Trying #{key} in SmartVar")
+          results = smartquery(fqdn, key)
+        end 
+        Hiera.debug("Found #{results}")
+        return results
+      end
+
+      def encquery (fqdn, key)
+        Hiera.debug("performing ENC lookup on #{fqdn} for #{key}")
+        answer = []
 
         foreman_uri      = URI.parse("#{@url}/node/#{fqdn}?format=yml")
         http             = Net::HTTP.new(foreman_uri.host, foreman_uri.port)
         http.use_ssl     = foreman_uri.scheme == 'https'
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE if http.use_ssl
         request          = Net::HTTP::Get.new(foreman_uri.request_uri)
+  
+        data = YAML.load(http.request(request).body)
 
-        node_definition = YAML.load(http.request(request).body)
         case key
-        when 'classes'
-          node_definition['classes'] || []
-        else
-          node_definition['parameters'][key] || nil
+          when 'classes'
+            Hiera.debug("Parsing classes")
+            answer = data['classes'] || []
+          else
+            Hiera.debug("Parsing something else")
+            answer = data['parameters'][key] || nil
+        end
+  
+        Hiera.debug("ENC returning #{answer}")
+        return answer
+      end
+
+      def smartquery (fqdn, key)
+        uri = URI.parse("#{@url}")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true if uri.scheme == 'https'
+
+        path = URI.escape("/hosts/#{fqdn}/lookup_keys/#{key}")
+        req = Net::HTTP::Get.new(path)
+        req.basic_auth("#{@user}", "#{@pass}")
+        req['Content-Type'] = 'application/json'
+        req['Accept'] = 'application/json'
+
+        begin
+          Timeout::timeout(5) { JSON.parse(http.request(req).body)["value"] }
+        rescue
+          Hiera.debug("no smart var for #{key}")
+          return nil
         end
       end
+  
     end
   end
 end
-
